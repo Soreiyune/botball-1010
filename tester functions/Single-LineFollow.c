ef419a44efe4102db442093986736a42f686d951
Soreiyune <34194682+Soreiyune@users.noreply.github.com>
2019-04-10 19:51:07 -0400

    Create Single-LineFollow.c

---
+++ tester functions/Single-LineFollow.c
@@ -0,0 +1,28 @@
+const int tophat = 0;
+const int black  = 2000;
+
+const int Rmotor = 0;
+const int Lmotor = 1;
+const int power  = 1000;
+
+void SinLine(int time){
+int t = seconds() + time
+while(t > seconds()){
+	
+	if(analog(tophat) < black){
+		mav(Rmotor, power-500);
+		mav(Lmotor, power);		
+		msleep(1);
+	}
+	else{
+		mav(Rmotor, power);
+		mav(Lmotor, power-500);
+		msleep(1);
+	}
+}
+
+}
+
+int main(){
+sinLine(4);
+}
